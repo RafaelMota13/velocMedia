776dc6b3741c66426048e09c6932829904882758
Rafael <faelmota7@gmail.com>
2022-09-23 22:14:48 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿double tempo, distancia, velocidade;
+
+Console.Write("\nDistância Percorrida (m): ");
+distancia = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Tempo Gasto (s): ");
+tempo = Convert.ToDouble(Console.ReadLine());
+
+velocidade = (distancia / tempo);
+
+Console.Write("\nVelocidade Média: ");
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.WriteLine($"{velocidade:N2} m/s\n");
+
+Console.ResetColor();
